647d50981c5e557a6837c8c1538e49d43dfbb592
DANNJUGUNA <ndan30071@gmail.com>
2023-02-14 06:41:18 +0300

    lab complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
   # write code here
-end
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1,side2, side3)
+    @side1= side1
+    @side2=side2
+    @side3 =side3
+  end
+  def kind 
+    if [side1,side2,side3].min <=0 || (side1+side2<=side3 || side1+side3<=side2|| side2+side3<=side1)
+      raise TriangleError
+    elsif side1==side2 && side3== side2
+      :equilateral
+    elsif side1==side2 || side2==side3|| side3==side1 
+      :isosceles
+    else
+        :scalene
+    end
+  end
+    
+  class TriangleError < StandardError
+    # triangle error code
+    
+  end
+
+  end
